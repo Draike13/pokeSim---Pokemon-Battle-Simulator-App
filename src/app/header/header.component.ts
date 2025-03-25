65ed015d02ee9bb969f9d04dfde6ed35882ae7fd
Trevor Biri <tmbiri91@gmail.com>
2025-03-25 15:57:16 -0500

    build reset game function to reset all screen elements to default when new game is called

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -13,6 +13,7 @@ export class HeaderComponent {
   constructor(private helperService: HelperService) {}
 
   startGame() {
+    this.resetGame();
     this.loadPlayerPokemon();
     this.loadComputerPokemon();
   }
@@ -23,5 +24,14 @@ export class HeaderComponent {
   loadComputerPokemon() {
     this.helperService.computerParty = [];
     return this.helperService.makeComputerParty();
+  }
+  resetGame() {
+    this.helperService.pokemon.map(
+      (eachPokemon) => (eachPokemon.selected = false)
+    );
+    this.helperService.playerBackground.set('transparent');
+    this.helperService.computerBackground.set('transparent');
+    this.helperService.playerBattlePokemon.set('');
+    this.helperService.computerBattlePokemon.set('');
   }
 }
